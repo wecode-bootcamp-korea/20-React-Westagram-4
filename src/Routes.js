929609202d4c8a7c6fa91acd31b7f263eb725587
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-04-28 17:48:53 +0900

    Modify: 초기 설정 수정 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginYM from "./pages/jeonyongmin/Login/Login";
-import MainYM from "./pages/jeonyongmin/Main/Main";
-import LoginJY from "./pages/leejiyon/Login/Login";
-import MainJY from "./pages/leejiyon/Main/Main";
-import LoginSM from "./pages/josungmin/Login/Login";
-import MainSM from "./pages/josungmin/Main/Main";
-import LoginYJ from "./pages/limyujin/Login/Login";
-import MainYJ from "./pages/limyujin/Main/Main";
-import LoginOJ from "./pages/kwonojae/Login/Login";
-import MainOJ from "./pages/kwonojae/Main/Main";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginYM from './pages/jeonyongmin/Login/Login';
+import MainYM from './pages/jeonyongmin/Main/Main';
+import LoginJY from './pages/leejiyon/Login/Login';
+import MainJY from './pages/leejiyon/Main/Main';
+import LoginSM from './pages/josungmin/Login/Login';
+import MainSM from './pages/josungmin/Main/Main';
+import LoginYJ from './pages/limyujin/Login/Login';
+import MainYJ from './pages/limyujin/Main/Main';
+import LoginOJ from './pages/kwonojae/Login/Login';
+import MainOJ from './pages/kwonojae/Main/Main';
 
 class Routes extends React.Component {
   render() {
