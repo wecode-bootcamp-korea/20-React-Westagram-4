778ddcf24ba6d5cfccbccc0863b237da24013145
jiyon <jiyonlee.lee@gmail.com>
2021-04-30 21:38:29 +0900

    Add: 댓글달기 기능 구현(댓글 좋아요 누르기, 엔터키 누를시 댓글달기, 댓글입력시 게시버튼 색 변경)

--- src/pages/leejiyon/Main/Components/Article.js
+++ src/pages/leejiyon/Main/Components/Article.js
@@ -2,25 +2,108 @@ import React, { Component } from 'react';
 import '../../../../styles/leejiyon/common.scss';
 
 const emptyHeartStyle = {
-  backgroundPosition: `-130px -478px`,
+  backgroundPosition: `-156px -478px`,
 };
 const likedHeartStyle = {
-  backgroundPosition: `-156px -478px`,
+  backgroundPosition: `-130px -478px`,
 };
 
 class Article extends Component {
+  inputRef = React.createRef();
   state = {
     isArticleLiked: false,
+    isCommentInputEntered: false,
+    commentInputValue: '',
+    currentUser: 'hao',
+    users: [
+      {
+        id: 'iu',
+        comment: '아자아자',
+        showTrashBtn: false,
+        liked: true,
+      },
+      {
+        id: 'jiyon',
+        comment: '공부를 하자',
+        showTrashBtn: false,
+        liked: false,
+      },
+      {
+        id: 'Shaman_king',
+        comment: '벨로그 쓰기',
+        showTrashBtn: false,
+        liked: false,
+      },
+    ],
   };
 
-  onClickBtn = btn => {
+  onClickArticleHeartBtn = () => {
     this.setState({
       isArticleLiked: !this.state.isArticleLiked,
     });
   };
 
+  onClickPostCommentInput = e => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      this.onClickPostBtn();
+    }
+  };
+
+  onChangeCommentInput = e => {
+    this.setState({
+      commentInputValue: e.target.value,
+    });
+    if (e.target.value.length > 0) {
+      this.setState({
+        isCommentInputEntered: true,
+      });
+    } else {
+      this.setState({
+        isCommentInputEntered: false,
+      });
+    }
+  };
+
+  onClickPostBtn = () => {
+    if (!this.state.commentInputValue.length) {
+      return;
+    }
+    this.setState({
+      users: this.state.users.concat({
+        id: this.state.currentUser,
+        comment: this.state.commentInputValue,
+        showTrashBtn: true,
+      }),
+      commentInputValue: '',
+    });
+    this.inputRef.current.focus();
+  };
+
+  onClickCommentHeartBtn = e => {
+    const currentCommentWrap = e.target.parentNode.parentNode;
+    const commentWraps = currentCommentWrap.parentNode.querySelectorAll(
+      '.commentWrap'
+    );
+    const clickedCommentIndex = Array.prototype.indexOf.call(
+      commentWraps,
+      currentCommentWrap
+    );
+
+    let users = [...this.state.users];
+    let user = { ...users[clickedCommentIndex - 1] };
+    if (user.liked) {
+      user.liked = false;
+    } else {
+      user.liked = true;
+    }
+    users[clickedCommentIndex - 1] = user;
+    this.setState({ users });
+  };
+
+  onClickDeleteBtn = e => {};
+
   render() {
-    console.log(this.state.isArticleLiked);
     return (
       <article className="Article">
         <div className="articleHeader">
@@ -42,7 +125,7 @@ class Article extends Component {
         <div className="articleLnb">
           <button
             className="lnbImg heartImg"
-            onClick={e => this.onClickBtn(e.target)}
+            onClick={e => this.onClickArticleHeartBtn(e.target)}
             style={
               this.state.isArticleLiked ? likedHeartStyle : emptyHeartStyle
             }
@@ -53,7 +136,6 @@ class Article extends Component {
         </div>
         <div className="articleComment">
           <div className="articleLikes">
-            <a href="#"></a>
             <span>wecode</span>님
             <span>
               외 <span className="likeNum">10</span>명
@@ -66,14 +148,35 @@ class Article extends Component {
               백두산 정상에 올라서 사진을 찍었다
             </span>
           </div>
-          <div className="commentWrap">
-            <span className="commenter">iu</span>
-            <span className="comment">거봐 좋았잖아 ~~~~ </span>
-            <button className="commentBtn commentLikesBtn">
-              <i className="emptyHeart far fa-heart"></i>
-              <i className="redHeart fas fa-heart red hide"></i>
-            </button>
-          </div>
+
+          {this.state.users.map(v => {
+            return (
+              <div className="commentWrap">
+                <span className="commenter">{v.id}</span>
+                <span className="comment">{v.comment}</span>
+                <button
+                  className="commentBtn commentDeleteBtn"
+                  onClick={e => {
+                    this.onClickDeleteBtn(e);
+                  }}
+                >
+                  {v.showTrashBtn ? <i className="far fa-trash-alt"></i> : null}
+                </button>
+                <button
+                  className="commentBtn commentLikesBtn"
+                  onClick={e => {
+                    this.onClickCommentHeartBtn(e);
+                  }}
+                >
+                  {v.liked ? (
+                    <i className="redHeart fas fa-heart red"></i>
+                  ) : (
+                    <i className="emptyHeart far fa-heart"></i>
+                  )}
+                </button>
+              </div>
+            );
+          })}
           <div className="commentTime">42분 전</div>
         </div>
         <div className="articlePostComment">
@@ -81,8 +184,26 @@ class Article extends Component {
             className="postCommentInput"
             type="text"
             placeholder="댓글 달기..."
+            value={this.state.commentInputValue}
+            onChange={e => {
+              this.onChangeCommentInput(e);
+            }}
+            onKeyUp={e => this.onClickPostCommentInput(e)}
+            ref={this.inputRef}
           />
-          <button className="postCommentBtn">게시</button>
+          <button
+            className="postCommentBtn"
+            onClick={e => {
+              this.onClickPostBtn(e);
+            }}
+            style={
+              this.state.isCommentInputEntered
+                ? { color: 'blue' }
+                : { color: '#72c2fa' }
+            }
+          >
+            게시
+          </button>
           <button className="smile">
             <i className="far fa-smile"></i>
           </button>
