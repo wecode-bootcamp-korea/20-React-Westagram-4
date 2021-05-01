11fe968edf172475bdd11485694f7db5f03ef528
yongminJeon <dydalsdl1414@gmail.com>
2021-05-01 14:35:49 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현(button active 기능 추가)

--- src/pages/jeonyongmin/Login/Login.js
+++ src/pages/jeonyongmin/Login/Login.js
@@ -6,19 +6,23 @@ import './Login.scss';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { id: '', password: '', buttonColor: false };
+    this.state = {
+      id: '',
+      password: '',
+      buttonColor: false,
+    };
   }
 
-  goToMain = () => {
+  goToMain = e => {
     this.props.history.push('./mainym');
   };
 
-  handleIdInput = e => {
-    this.setState({ id: e.target.value }, this.validation);
-  };
-
-  handlePasswordInput = e => {
-    this.setState({ password: e.target.value }, this.validation);
+  handleIdPwInput = e => {
+    if (e.target.type === 'text') {
+      this.setState({ id: e.target.value }, this.validation);
+    } else if (e.target.type === 'password') {
+      this.setState({ password: e.target.value }, this.validation);
+    }
   };
 
   validation = () => {
@@ -28,35 +32,35 @@ class Login extends React.Component {
   };
 
   render() {
-    const { buttonColor } = this.state;
+    const { id, password, buttonColor } = this.state;
+    const condtionOfButtonActivated = id.includes('@') && password.length >= 5;
     return (
       <main className="login">
         <div className="loginPage">
           <h1 className="loginTitle">Westagram</h1>
           <div className="loginContents">
-            <form
-              action="./main.html"
-              method="get"
-              className="loginInformation"
-            >
+            <form action="#" method="get" className="loginInformation">
               <input
                 className="loginInputId input"
                 type="text"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
                 required
-                onChange={this.handleIdInput}
+                onChange={this.handleIdPwInput}
               />
               <input
                 className="loginInputPassword input"
                 type="password"
                 placeholder="비밀번호"
                 required
-                onChange={this.handlePasswordInput}
+                onChange={this.handleIdPwInput}
               />
               <button
-                className={buttonColor ? 'loginButtonActive' : 'loginButton'}
-                type="submit"
+                className={
+                  buttonColor ? 'loginButtonActive' : 'loginButtonDeactive'
+                }
+                type="subtmit"
                 onClick={this.goToMain}
+                disabled={condtionOfButtonActivated ? false : true}
               >
                 로그인
               </button>
