8516630072db3a060c8fdb307ab304fe3fecfe0b
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-03 13:55:56 +0900

    Add : mockData

--- src/pages/limyujin/Main/Components/MainRight.js
+++ src/pages/limyujin/Main/Components/MainRight.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 class MainRight extends React.Component {
   render() {
-    const sampleText = ['계정아이디', '계정아이디', '계정아이디', '계정아이디'];
+    const { accountInfo } = this.props;
     return (
       <>
         <aside className="main-right">
@@ -25,21 +25,27 @@ class MainRight extends React.Component {
             </div>
             <div>
               <ul>
-                {sampleText.map(el => (
+                {accountInfo.slice(0, 4).map(el => (
                   <li className="main-right-section__item-row">
                     <div className="main-right-section__user-img-box">
-                      <div className="circle-profile__story-border circle-profile__story-border--main-right-user"></div>
+                      <div
+                        className={
+                          el.story
+                            ? 'circle-profile__story-border circle-profile__story-border--main-right-user'
+                            : ''
+                        }
+                      ></div>
                       <img
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=fZBB-70QdV0AX9iZy7F&edm=ABfd0MgAAAAA&ccb=7-4&oh=55dfd4c5aa0657dcdf5cf7c0a098157d&oe=60A452F0&_nc_sid=7bff83"
-                        alt=""
+                        src={el.profileImg}
+                        alt="`${el.author}님의 이미지`"
                         className="circle-profile circle-profile--main-right-user"
                       />
                     </div>
                     <div className="main-right-section__texts">
                       <span className="main-right-section__account-id">
-                        {el}
+                        {el.author}
                       </span>
-                      <span>16분전</span>
+                      <span>{el.time}</span>
                     </div>
                   </li>
                 ))}
@@ -53,26 +59,31 @@ class MainRight extends React.Component {
             </div>
             <div>
               <ul>
-                {sampleText.map(el => (
-                  <li className="main-right-section__item-row">
-                    <div className="main-right-section__user-img-box">
-                      <img
-                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/64219646_866712363683753_7365878438877462528_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=fZBB-70QdV0AX9iZy7F&edm=ABfd0MgAAAAA&ccb=7-4&oh=55dfd4c5aa0657dcdf5cf7c0a098157d&oe=60A452F0&_nc_sid=7bff83"
-                        alt=""
-                        className="circle-profile circle-profile--main-right-user"
-                      />
-                    </div>
-                    <div className="main-right-section__texts">
-                      <span className="main-right-section__account-id">
-                        {el}
-                      </span>
-                      <span>Instagram 신규 가입</span>
-                    </div>
-                    <button className="main-right-section__recommend-follow">
-                      팔로우
-                    </button>
-                  </li>
-                ))}
+                {accountInfo
+                  .reverse()
+                  .slice(0, 4)
+                  .map(el => (
+                    <li className="main-right-section__item-row">
+                      <div className="main-right-section__user-img-box">
+                        <img
+                          src={el.profileImg}
+                          alt="`${el.author}님의 이미지`"
+                          className="circle-profile circle-profile--main-right-user"
+                        />
+                      </div>
+                      <div className="main-right-section__texts">
+                        <span className="main-right-section__account-id">
+                          {el.author}
+                        </span>
+                        <span>
+                          {el.newAccount ? 'Instagram 신규 가입' : ''}
+                        </span>
+                      </div>
+                      <button className="main-right-section__recommend-follow">
+                        팔로우
+                      </button>
+                    </li>
+                  ))}
               </ul>
             </div>
           </section>
--- src/pages/limyujin/Main/Main.js
+++ src/pages/limyujin/Main/Main.js
@@ -8,43 +8,20 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      feedInfo: [
-        {
-          author: 'rilip_official',
-          profileImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/94003553_231199768097287_5780010293664940032_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=EjeCkaUjMMgAX9m0Dym&edm=AEF8tYYAAAAA&ccb=7-4&oh=70612d83cfbfb530e39c0695f4af134b&oe=60A4EFD6&_nc_sid=a9513d',
-          location: 'suwon',
-          mainImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/175260006_501085961251601_2268575138294333749_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=RJE0N2r6OsQAX8n_B-z&edm=AIQHJ4wAAAAA&ccb=7-4&oh=25fe99fccac6c74f937965f44b445884&oe=60A31BE1&_nc_sid=7b02f1',
-          text: '위워크에서 진행한 베이크 클래스',
-          time: '17시간 전',
-          like: 70,
-        },
-        {
-          author: 'krtigersugar',
-          profileImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/43778288_543710792708920_7607289634233516032_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=pWeqQfWFVmcAX_kmyQF&edm=AEF8tYYAAAAA&ccb=7-4&oh=4ec3ccefbd37ce11dc957b7838b77d05&oe=60A634E6&_nc_sid=a9513d',
-          location: '',
-          mainImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/175810080_497894738013789_23924597793451987_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=109&_nc_ohc=Tu_Wnsa9tiwAX80ztR3&edm=AIQHJ4wAAAAA&ccb=7-4&oh=d91ccc0c9d232e280584d62e6c2b4652&oe=60A513B3&_nc_sid=7b02f1',
-          text: '✨신메뉴 소문내기 이벤트✨',
-          time: '19시간 전',
-          like: 34,
-        },
-        {
-          author: 'anko_8090',
-          profileImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/163663946_197803795082398_1924950408015660072_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=Jvc5nBmQBVYAX-SQowW&edm=AEF8tYYAAAAA&ccb=7-4&oh=f33f3a525c2908fd436991a6de94643d&oe=60A7CC97&_nc_sid=a9513d',
-          location: '',
-          mainImg:
-            'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/176282393_1404333343292706_135428849258000862_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=tH6ZNMQ8JUoAX9tGaTC&tn=E3vZwT1zJnhTn-R6&edm=AIQHJ4wAAAAA&ccb=7-4&oh=53efa849b2a73835e8ea08a5f35ce7dc&oe=60A6C3FF&_nc_sid=7b02f1',
-          text: '알박기 제법...',
-          time: '20시간 전',
-          like: 13,
-        },
-      ],
+      feedInfo: [],
     };
   }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/limyujin/westaData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ feedInfo: data });
+      });
+  }
+
   render() {
     const { feedInfo } = this.state;
 
@@ -62,11 +39,12 @@ class Main extends React.Component {
                 text={el.text}
                 time={el.time}
                 like={el.like}
+                story={el.story}
                 key={index}
               />
             ))}
           </div>
-          <MainRight />
+          <MainRight accountInfo={feedInfo} />
         </main>
       </>
     );
