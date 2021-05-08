fdf8d6b361cca3ff338eeea751c6f6f1d31569de
Emewjin <76927618+Emewjin@users.noreply.github.com>
2021-05-09 00:50:40 +0900

    Fix : Conflict 해결

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './styles/common.scss';
 import './styles/reset.scss';
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
