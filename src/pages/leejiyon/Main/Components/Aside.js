cb9a695572e73141caaed163ef0e1fa980540bcd
jiyonlee <jiyonlee.d@gmail.com>
2021-05-09 17:07:07 +0900

    Modify: Aside.js 목데이터 분리

--- src/pages/leejiyon/Main/Components/Aside.js
+++ src/pages/leejiyon/Main/Components/Aside.js
@@ -2,6 +2,39 @@ import React, { Component } from 'react';
 import AsideContainer from './AsideContainer';
 import './Aside.scss';
 
+const STORY_LIST = [
+  {
+    id: 1,
+    imgAlt: '가아라',
+    imgSrc: '/images/leejiyon/gaara.jpg',
+    profileId: 'gaara',
+    postTime: '1분 전',
+  },
+  {
+    id: 2,
+    imgAlt: '효신',
+    imgSrc: '/images/leejiyon/hyoshin.jpeg',
+    profileId: 'hyoshin',
+    postTime: '2분 전',
+  },
+];
+const RECOMMEND_LIST = [
+  {
+    id: 11,
+    imgAlt: '록리',
+    imgSrc: '/images/leejiyon/lock-li.jpg',
+    profileId: 'lock-li',
+    postTime: '20분 전',
+  },
+  {
+    id: 22,
+    imgAlt: '나루토',
+    imgSrc: '/images/leejiyon/naruto.jpg',
+    profileId: 'naruto',
+    postTime: '16분 전',
+  },
+];
+
 class Aside extends Component {
   render() {
     return (
@@ -20,42 +53,12 @@ class Aside extends Component {
         <AsideContainer
           title="스토리"
           content="모두 보기"
-          profileLists={[
-            {
-              id: 1,
-              imgAlt: '가아라',
-              imgSrc: '/images/leejiyon/gaara.jpg',
-              profileId: 'gaara',
-              postTime: '1분 전',
-            },
-            {
-              id: 2,
-              imgAlt: '효신',
-              imgSrc: '/images/leejiyon/hyoshin.jpeg',
-              profileId: 'hyoshin',
-              postTime: '2분 전',
-            },
-          ]}
+          profileLists={STORY_LIST}
         />
         <AsideContainer
           title="회원님을 위한 추천"
           content="모두 보기"
-          profileLists={[
-            {
-              id: 11,
-              imgAlt: '록리',
-              imgSrc: '/images/leejiyon/lock-li.jpg',
-              profileId: 'lock-li',
-              postTime: '20분 전',
-            },
-            {
-              id: 22,
-              imgAlt: '나루토',
-              imgSrc: '/images/leejiyon/naruto.jpg',
-              profileId: 'naruto',
-              postTime: '16분 전',
-            },
-          ]}
+          profileLists={RECOMMEND_LIST}
         />
       </aside>
     );
